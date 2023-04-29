9e665dd94640e39060b2c3f7830e6656bf264840
apostoll13th <lowtip13@gmail.com>
2023-04-29 16:56:38 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,98 @@
+import { Telegraf, session } from "telegraf";
+import config from 'config'
+
+import {message} from "telegraf/filters";
+import { ogg } from "./ogg.js";
+import { openai } from "./openai.js";
+import {code} from "telegraf/format"
+
+let count = 0;
+let INITIAL_SESSION = {
+  messages: [],
+}
+
+
+const bot = new Telegraf(config.get('TELEGRAM_API'))
+
+bot.use(session())
+
+bot.command('new', async (ctx) => {
+  ctx.session = INITIAL_SESSION
+  INITIAL_SESSION.messages = []
+  await ctx.reply(code("Жду сообщения или текста от тебя друг мой"))
+})
+
+
+bot.on(message('voice'), async (ctx) =>{
+  try{
+    ctx.session ??= INITIAL_SESSION
+    await ctx.telegram.sendChatAction(ctx.chat.id, "typing");
+    count = count + 1
+    console.log(count);
+    await ctx.reply(code("Сообщение принял, жди и мозги не еби"))
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+    const userId = String(ctx.message.from.id)
+    const oggPath = await ogg.create(link, userId)
+    const mp3 = await ogg.toMP3(oggPath, userId)
+    const text = await openai.transcription(mp3) 
+    
+
+    ctx.reply(code(`Твой запрос выглядит так: ${text}`))
+
+    ctx.session.messages.push({role: openai.roles.USER, content: text})
+    const response = await openai.chat(ctx.session.messages)
+    
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content
+    })
+    await ctx.reply(response.content)
+
+
+  } catch(e) {
+     console.log('Error voice -  ${e}')
+  }
+} )
+
+
+bot.on(message('text'), async (ctx) => {
+  try{ 
+    ctx.session ??= INITIAL_SESSION
+    if (ctx.session.blocked) {
+      return;
+    }
+    ctx.session.blocked = true
+    await ctx.telegram.sendChatAction(ctx.chat.id, "typing");
+    
+    count = count + 1
+    console.log(count);
+    await ctx.reply(code("Сообщение принял, жди и мозги не еби"))
+  //   const messages = [{
+  //   role:openai.roles.USER,
+  //   content: ctx.message.text
+  // }]
+  ctx.session.messages.push({role: openai.roles.USER, content: ctx.message.text})
+  const response = await openai.chat(ctx.session.messages)
+  
+  ctx.session.messages.push({
+    role: openai.roles.ASSISTANT,
+    content: response.content
+  })
+  await ctx.reply(response.content)
+  ctx.session.blocked = false
+}
+catch (e) {
+  console.log(e.message, "error with text message")
+}
+})
+
+bot.command('start', async (ctx) => {
+  ctx.session = INITIAL_SESSION
+  await ctx.reply(code("Жду сообщения или текста от тебя друг мой"))
+})
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+
---
+++ src/ogg.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import ffmpeg from "fluent-ffmpeg";
+import { createWriteStream } from "fs";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import installer from '@ffmpeg-installer/ffmpeg'
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+class OggConverter {
+  constructor() {
+    ffmpeg.setFfmpegPath(installer.path)
+  }
+
+  async create(url, fileName) {
+    try {
+      const oggPath = resolve(__dirname, "../voice/", `${fileName}.ogg`);
+
+      const response = await axios({
+        method: "get",
+        url,
+        responseType: "stream",
+      });
+      return new Promise((resolve) => {
+        const stream = createWriteStream(oggPath);
+        response.data.pipe(stream)
+        stream.on('finish', () => resolve(oggPath) )
+      });
+    } catch (error) {
+      console.log("error with create file", error.message);
+    }
+  }
+
+  toMP3(input, output) {
+    try {   
+    const outputPath = resolve(dirname(input), `${output}.mp3`)
+    return new Promise((resolve, reject) => {
+      ffmpeg(input)
+        .inputOption('-t 30')
+        .output(outputPath)
+        .on('end', () => resolve(outputPath))
+        .on('error', (error) => reject(error.message))
+        .run()
+    })
+    } catch (error) {
+        console.log('error with create mp3', error.message)
+    }
+  }
+}
+
+export const ogg = new OggConverter();
